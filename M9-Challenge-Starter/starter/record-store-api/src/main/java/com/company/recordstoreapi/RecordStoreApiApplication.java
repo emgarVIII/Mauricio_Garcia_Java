c2dd712dfd3943967edaf3875fc9e1f29a1139d5
Mauricio Garcia <mauriciogarcia@Mauricios-MacBook-Pro.local>
2023-08-17 10:16:02 -0500

    first commit

---
+++ M9-Challenge-Starter/starter/record-store-api/src/main/java/com/company/recordstoreapi/RecordStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.company.recordstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordStoreApiApplication.class, args);
+	}
+
+}
