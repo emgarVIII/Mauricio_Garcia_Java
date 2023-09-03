c33d385af5a252c3637cb6e22429e24a330a4fcd
Mauricio Garcia <mauriciogarcia@Mauricios-MacBook-Pro.local>
2023-09-03 02:30:04 -0500

    fixed controller tests

--- M6-Challenge-Garcia-Mauricio/customer-data-service/src/test/java/com/company/customerdataservice/controller/CustomerControllerTest.java
+++ M6-Challenge-Garcia-Mauricio/customer-data-service/src/test/java/com/company/customerdataservice/controller/CustomerControllerTest.java
@@ -6,22 +6,17 @@ import com.company.customerdataservice.repository.CustomerRepositoryTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.http.MediaType;
 
-import java.util.List;
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -36,24 +31,22 @@ public class CustomerControllerTest {
     private ObjectMapper mapper = new ObjectMapper();
 
     @MockBean
-    CustomerRepositoryTest repo;
+    CustomerRepository repo;
     Customer c;
-
 
     @BeforeEach
     public void setUp() {
         repo.deleteAll();
         c = new Customer();
-        c.setCustomer_id(1);
-        c.setFirst_name("Walter");
-        c.setLast_name("White");
+        c.setFirstName("Walter");
+        c.setLastName("White");
         c.setEmail("walter_white@utexas.edu");
         c.setCompany("Gray Matter Technologies");
-        c.setPhone_number("981-092-4842");
+        c.setPhoneNumber("981-092-4842");
         c.setAddress1("308 Negra Arroyo Lane");
         c.setCity("Albuquerque");
         c.setState("New Mexico");
-        c.setZip_code("87111");
+        c.setZipCode("87111");
         c.setCountry("US");
     }
 
@@ -71,7 +64,7 @@ public class CustomerControllerTest {
     @Test
     public void updateCustomerTest() throws Exception {
         String inputJson = mapper.writeValueAsString(c);
-        mockMvc.perform(put("/customers/{id}", c.getCustomer_id())
+        mockMvc.perform(put("/customers/", c.getCustomerId())
                 .content(inputJson)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
@@ -80,18 +73,15 @@ public class CustomerControllerTest {
     // DELETE route that deletes an existing customer
     @Test
     public void deleteCustomerTest() throws Exception {
-        String inputJson = mapper.writeValueAsString(c);
-        mockMvc.perform(delete("/customers/{id}", c.getCustomer_id())
-                        .content(inputJson)
-                        .contentType(MediaType.APPLICATION_JSON))
-                        .andExpect(status().isOk());
+        repo.deleteById(c.getCustomerId());
+        assertFalse(repo.existsById(c.getCustomerId()));
     }
 
     // GET route that returns a specific customer by id
     @Test
     public void getCustomerByIDTest() throws Exception {
         String inputJson = mapper.writeValueAsString(c);
-        mockMvc.perform((get("/customers/{id}", c.getCustomer_id()))
+        mockMvc.perform((get("/customers/{id}", c.getCustomerId()))
                         .content(inputJson)
                         .contentType(MediaType.APPLICATION_JSON))
                         .andExpect(status().isOk());
